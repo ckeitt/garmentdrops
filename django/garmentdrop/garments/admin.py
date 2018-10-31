af4bafe2597529395f72cc1234fef39d7c97cf4b
Caleb Keitt <calebkeitt94@gmail.com>
2018-10-31 19:59:58 -0400

    segmented base garment with specific garment type & size as well as additional class method

--- django/garmentdrop/garments/admin.py
+++ django/garmentdrop/garments/admin.py
@@ -3,12 +3,18 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from garments.models import BaseGarment
 from garments.models import Garment
 from garments.models import Colorway
 from garments.models import GarmentSize
 from garments.models import GarmentGender
 from garments.models import GarmentFabric
 from garments.models import GarmentImage
+from garments.models import GarmentType
+
+@admin.register(BaseGarment)
+class BaseGarmentAdmin(admin.ModelAdmin):
+    pass
 
 @admin.register(Garment)
 class GarmentAdmin(admin.ModelAdmin):
@@ -16,6 +22,10 @@ class GarmentAdmin(admin.ModelAdmin):
 
 @admin.register(Colorway)
 class ColorwayAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(GarmentType)
+class GarmentTypeAdmin(admin.ModelAdmin):
     pass
 
 @admin.register(GarmentSize)
